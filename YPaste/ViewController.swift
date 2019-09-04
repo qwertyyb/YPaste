289a19d833700e343af12324be2c54fc3e237f1d
qwertyyb <qwertyyb@qq.com>
2019-09-05 01:02:33 +0800

    Initial Commit

---
+++ YPaste/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YPaste
+//
+//  Created by 虚幻 on 2019/9/5.
+//  Copyright © 2019 qwertyyb. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
